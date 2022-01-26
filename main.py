9c16ff44c21f49bce62c2d58fe6fbc89aaf4e04a
Micha Berdichevsky <berdim99@gmail.com>
2022-01-25 22:03:55 -0500

    use words list from the wordle webiste

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# Press ⌃R to execute it or replace it with your code.
 from typing import List, Dict, Tuple
 
 WORD_LENGTH = 5  # can be max of 9
 MAX_WORDS_TO_SHOW = 30
-WORDS_SOURCE = "/usr/share/dict/words"
+# WORDS_SOURCE = "/usr/share/dict/words"
+WORDS_SOURCE = "words.txt"
 
 
 class State:
